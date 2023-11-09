bcea8484ec96a3c62829a39485b2fd766e716de2
G-SushmaReddy <reddysushma2004@gmai.com>
2023-11-10 02:29:35 +0530

    First commit

---
+++ src/main/java/com/app/java/PresentProject/App.java
@@ -0,0 +1,13 @@
+package com.app.java.PresentProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
